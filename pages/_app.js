2ca8353d42c3124a690622ab078974abe4986a44
Michael Malleske <mmalleske@gmail.com>
2023-10-16 13:17:27 -0700

    prevent user scaling

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps }) {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
           <link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital@0;1&family=Quicksand:wght@300;400;500&family=Work+Sans:ital,wght@0,100;0,200;1,100&display=swap" rel="stylesheet"></link>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         </Head>
         <Component {...pageProps} />
       </ConfigProvider>
